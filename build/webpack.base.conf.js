39935189540f3dbe76c7c4f181bbaac1c58e8f0b
afeiship <1290657123@qq.com>
2017-03-29 17:39:13 +0800

    optmize

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -11,9 +11,10 @@ var cssSourceMapProd = (env === 'production' && config.build.productionSourceMap
 var useCssSourceMap = cssSourceMapDev || cssSourceMapProd;
 var externals = process.env.NODE_ENV === 'production' ? {
   react: 'React',
-  classnames: 'classNames',
+  classnames: 'classnames',
   'react-dom': 'ReactDOM',
   'noop': 'noop',
+  'react-lazyimg': 'ReactLazyimg',
 } : {};
 
 module.exports = {
